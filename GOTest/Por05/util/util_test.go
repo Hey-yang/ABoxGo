c201ccb19ac9f9126b59a49fdd89864af137f6b6
杨逢春 <517052727@qq.com>
2023-05-06 21:25:59 +0800

    调整了一下

---
+++ GOTest/Por05/util/util_test.go
@@ -0,0 +1,18 @@
+package util
+
+import "testing"
+
+// 功能测试
+func TestParseSize(t *testing.T) {
+
+}
+
+// 模糊测试
+func FuzzParseSize(f *testing.F) {
+
+}
+
+// 基准测试
+func BenchmarkParseSize(b *testing.B) {
+
+}
