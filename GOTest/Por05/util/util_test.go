ac4247b5c5771090139cba5dc93787752f42f8b4
杨逢春 <517052727@qq.com>
2023-05-07 23:58:48 +0800

    调整

--- GOTest/Por05/util/util_test.go
+++ GOTest/Por05/util/util_test.go
@@ -4,49 +4,27 @@ import (
 	"testing"
 )
 
-type commStruct struct {
-	Group         string
-	SizeStr       string
-	ExpectSize    int64
-	ExpectSizeStr string
-}
-
-var commTestData []commStruct
-
 // 功能测试
 func TestParseSize(t *testing.T) {
+	if value := Add(1, 4); value != 3 {
+		t.Errorf("1+2 expected be 3, but %d got", value)
+	}
 
+	if value := Mul(1, 2); value != 2 {
+		t.Errorf("1*2 expected be 2, but %d got", value)
+	}
 }
 
 // 模糊测试
 func FuzzParseSize(f *testing.F) {
-
+	f.Skip("模糊测试")
 }
 
 // 基准测试
 func BenchmarkParseSize(b *testing.B) {
-
+	b.Skip("基准测试")
 }
 
 func TestMain(m *testing.M) {
-
-}
-
-func initCommonData() {
-	commTestData = []commStruct{
-		{"B", "1b", B, "1B"},
-		{"B", "100b", 100 * B, "1B"},
-		{"KB", "1kb", KB, "1B"},
-		{"KB", "100kb", 100 * KB, "1B"},
-		{"MB", "1mb", 100, "1B"},
-		{"MB", "100mb", 100 * MB, "1B"},
-		{"GB", "1gb", 100, "1B"},
-		{"GB", "10gb", 100 * GB, "1B"},
-		{"TB", "1tb", 100, "1B"},
-		{"TB", "100tb", 100 * TB, "1B"},
-		{"PB", "1pb", 100, "1B"},
-		{"PB", "100pb", 100 * PB, "1B"},
-		{"UNKEOWMB", "1k", 2 * KB, "1B"},
-		{"UNKEOWMB", "1g", 2 * KB, "1B"},
-	}
+	m.Run()
 }
